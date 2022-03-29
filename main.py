9c463fc021abb72e2a91ac5257b2f87f0f7d2aa7
Gideon <balogungideon12@gmail.com>
2022-03-29 12:13:02 +0100

    tictactoe game

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import random
 from time import sleep
 
+played_keys = [] #keeps track of all the entered numbers
+player_scores =[0,0] # keeps track of player scores
 
-
-played_keys = []
 
 INSTRUCTIONS = "Here are the rules to the game:" \
                "RULES FOR TIC-TAC-TOE " \
@@ -15,6 +16,7 @@ INSTRUCTIONS = "Here are the rules to the game:" \
                "11 stands for row 1 column 1 which is the topmost left most position and 33 stands for the last position\n" \
                "which is the bottommost rightmost position and 22 stands for the second row of the second column\n" \
                "which is dead centre of the board.\n" \
+               "Position guide below:\n" \
                "[11, 12, 13]\n" \
                "[21, 22, 23]\n" \
                "[31, 32, 33]\n" \
@@ -22,6 +24,9 @@ INSTRUCTIONS = "Here are the rules to the game:" \
 
 
 def update_board(player, played, board):
+    """ takes in 3 arguments, the player, the number played and the tictactoe array and updates
+    the position with either 'X' or 'O'
+    returns the updated board"""
     pos_num_1 = int(played[0])
     pos_num_2 = int(played[1])
     if player == "player_1":
@@ -31,15 +36,25 @@ def update_board(player, played, board):
 
     return board
 
+def display_board(board):
+    """displays the board in presentable print format"""
+    print(f"---------------------------")
+    print(f"   {board[0][0]}    |    {board[0][1]}    |   {board[0][2]}   ")
+    print(f"---------------------------")
+    print(f"   {board[1][0]}    |    {board[1][1]}    |   {board[1][2]}   ")
+    print(f"---------------------------")
+    print(f"   {board[2][0]}    |    {board[2][1]}    |   {board[2][2]}   ")
+    print(f"---------------------------")
+
 
 def first_player(board):
+    """ takes in a single argument(the game array) and controls player 1 behaviour"""
     player_1 = input("player 1's turn: ")
     player = "player_1"
     result = check_valid_keys(player_1)
     if result == "good":
         update_board(player, player_1, board)
-        for i in range(3):
-            print(board[i])
+        display_board(board)
 
     else:
         print("can't play that")
@@ -47,22 +62,25 @@ def first_player(board):
 
 
 def second_player(board):
+    """ takes in a single argument(the game array) and controls player 2 behaviour"""
     player_2 = input("player 2's turn: ")
     player = "player_2"
     result = check_valid_keys(player_2)
     if result == "good":
         update_board(player, player_2, board)
-        for i in range(3):
-            print(board[i])
+        display_board(board)
 
     else:
         print("can't play that")
         second_player(board)
 
-
 def check_valid_keys(key):
+    """ takes in one argument; user input and validates it. returns either 'good' or 'bad' """
     valid_keys = [11, 12, 13, 21, 22, 23, 31, 32, 33]
+
     try:
+
+        print(played_keys)
         if int(key) in valid_keys and key not in played_keys:
             played_keys.append(key)
             return "good"
@@ -73,18 +91,16 @@ def check_valid_keys(key):
 
 
 def draw(box):
-    a = box[0].count("-")
-    b = box[1].count("-")
-    c = box[2].count("-")
-
-    # print("a", a)
-    # print("b", b)
-    # print("c", c)
+    """takes in one argument (game array) and checks the condition for a tie. returns True or False"""
+    a = box[0].count(" ")
+    b = box[1].count(" ")
+    c = box[2].count(" ")
 
     return (a + b + c) == 0
 
 
 def p1_win_conditions(board):
+    """takes in one argument (game array) and checks if condition for a win are met. returns True or False"""
     pos_1 = board[0][0]
     pos_2 = board[0][1]
     pos_3 = board[0][2]
@@ -114,6 +130,7 @@ def p1_win_conditions(board):
 
 
 def p2_win_conditions(board):
+    """takes in one argument (game array) and checks if condition for a win are met. returns True or False"""
     pos_1 = board[0][0]
     pos_2 = board[0][1]
     pos_3 = board[0][2]
@@ -142,45 +159,56 @@ def p2_win_conditions(board):
         return True
 
 
-def keep_scores():
-    player_1_score = 0
-    player_2_score = 0
-    return f""
+def display_scores(scores):
+    """formats player scores in print format"""
+    print(f"player one  {scores[0]}  |  {scores[1]} player two")
+
 
 def play_game():
-    print(INSTRUCTIONS)
+    """ contains the logical flow of the game. takes no argument"""
     consent = input("Play a game of X and O? Y/N ").lower()
     if consent == "y":
-        main_box = [["-", "-", "-"]
-            , ["-", "-", "-"],
-                    ["-", "-", "-"]]
-        for i in main_box:
-            print(i)
+        print(INSTRUCTIONS)
+        main_box = [[" ", " ", " "]
+            , [" ", " ", " "],
+                    [" ", " ", " "]]
+        played_keys.clear()
+
+        display_board(main_box)
         game_on = True
         while game_on:
+            display_scores(player_scores)
 
             first_player(main_box)
-            if p1_win_conditions(main_box):
-                print("player 1 wins")
+            if draw(main_box):
+                print("it's a tie 🤜🤛")
+                display_scores(player_scores)
                 game_on = False
-                sleep(2)
+                sleep(4)
                 play_game()
+
+            elif p1_win_conditions(main_box):
+                print("player 1 wins 🎉🎉👏👏")
+                player_scores[0] += 1
+                display_scores(player_scores)
+                game_on = False
+                sleep(4)
+                play_game()
+
 
             else:
                 second_player(main_box)
                 if p2_win_conditions(main_box):
-                    print("player 2 wins")
+                    print("player 2 wins 🎉🎉👏👏")
+                    player_scores[1] += 1
+                    display_scores(player_scores)
                     game_on = False
-                    sleep(2)
+                    sleep(4)
                     play_game()
 
-            if draw(main_box):
-                print("it's a draw")
-                game_on = False
-                sleep(2)
-                play_game()
+
     else:
-        print("hope you play next time")
+        print("come back next time")
         return
 
 
